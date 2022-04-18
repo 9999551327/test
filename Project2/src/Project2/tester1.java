fc4ea35d214f1a1bdba7af5346b4f834b6ca747e
ankit.sachan <ankit.sachan@MPL-DCF7573.magicsw.com>
2022-04-19 00:27:59 +0530

    pushing changes

--- Project2/src/Project2/tester1.java
+++ Project2/src/Project2/tester1.java
@@ -1,6 +1,7 @@
 package Project2;
 public class tester1 {
 	int a;
+	int b;
 	public void method12()
 	{
 		System.out.println("here is the method");
@@ -8,7 +9,9 @@ public class tester1 {
 	public static void main(String[] args) {
 		tester1 an=new tester1();
 		an.a=233;
+		an.b=5;
 		System.out.println(an.a);
+		System.out.println(an.b);
 		an.method12();
 	}
 
