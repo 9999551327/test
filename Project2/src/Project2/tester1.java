a920b592f312854d26aa2e579d969c893889c718
ankit.sachan <ankit.sachan@MPL-DCF7573.magicsw.com>
2022-04-19 00:19:42 +0530

    complte test1

---
+++ Project2/src/Project2/tester1.java
@@ -0,0 +1,15 @@
+package Project2;
+public class tester1 {
+	int a;
+	public void method12()
+	{
+		System.out.println("here is the method");
+	}
+	public static void main(String[] args) {
+		tester1 an=new tester1();
+		an.a=233;
+		System.out.println(an.a);
+		an.method12();
+	}
+
+}
